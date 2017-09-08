a212136a0099bec4537077c89838a7ec25b44edf
lilinrui <729977402@qq.com>
2017-09-08 13:19:29 +0800

    exercise

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# vue Demo
+# Vue2.0-ShoppingCart.rar
 
 > A Vue.js Demo
 ## explain
 
-这个Demo可以有效的了解vue中的一些基本命令和数据双向绑定
+这个Demo可以有效的了解vue中的一些基本命令和数据双向绑定等操作
+
+
+# VuexExercise
+
+> A Vuex.js Demo
+
+## explain
+
+Vuex初步了解..
---
+++ VuexExercise/src/components/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    count Is {{$store.state.count}}
+    <button @click="increment">+5</button>
+    <button @click="decrement">-3</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  computed: mapGetters([
+    'doneTodosCount'
+  ]),
+  methods: mapActions([
+    'increment',
+    'decrement'
+  ])
+}
+</script>
+
+<style scoped>
+
+</style>
